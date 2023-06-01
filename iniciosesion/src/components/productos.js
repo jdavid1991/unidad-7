a26df68ad01174a1a05b0282184dfbbe3b007d07
jdavid1991 <jrrdriwtf@gmail.com>
2023-05-31 21:00:15 -0500

    up

--- iniciosesion/src/components/productos.js
+++ iniciosesion/src/components/productos.js
@@ -5,35 +5,41 @@ function Productos() {
 
   const ListadoProductos = [
     {
+      id: 1,
       name: "Hamburguesa Sencilla",
       url:
         "https://homeburgers.com/uploads/product/054bb48d12d44687b47c00b41ab31097/hs5.jpg",
       release_year: "Hamburguesa"
     },
     {
+      id: 2,
       name: "Pizza Carne",
       url:
         "https://tofuu.getjusto.com/orioneat-prod-resized/gjPJC7hhyL5modera-1200-1200.webp",
       release_year: "Pizza"
     },
     {
+      id: 3,
       name: "Perro Sencillo",
       url:
         "https://d1kxxrc2vqy8oa.cloudfront.net/wp-content/uploads/2019/04/05085017/RFB-2603-4-perroscalientes.jpg",
       release_year: "Perro"
     },
     {
+      id: 4,
       name: "Pizza Hawaina",
       url:
         "https://napolicartagena.com/wp-content/uploads/2022/01/La-Pizza-Hawaiana-de-Canada.jpg",
       release_year: "Pizza"
     },
     {
+      id: 5,
       name: "Hamburguesa Doble carne",
       url: "https://cdn.pixabay.com/photo/2022/08/31/10/17/burger-7422970_1280.jpg",
       release_year: "Hamburguesa"
     },
     {
+      id: 6,
       name: "Pizza Pollo-champiñon",
       url: "https://images.hola.com/imagenes/cocina/recetas/20200123158802/receta-pizza-pollo-barbacoa/0-772-922/pizza-pollo-adobe-m.jpg",
       release_year: "Pizza"
@@ -136,6 +142,7 @@ function Productos() {
           <div className='car-item' key={index}>
             <div className='car-name'>{`${item.name}`}</div>
             <img className='car-image'src={item.url}/>
+            <button>Detalles</button>
           </div>
         ))}
       </div>
